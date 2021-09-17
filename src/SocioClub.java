62e2f1659feaa060d72a0bc2c6d812060a7720f4
JavierHdez <compartelo007@yahoo.es>
2021-09-17 20:57:16 +0100

    add resource doc exam

--- src/SocioClub.java
+++ src/SocioClub.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class SocioClub {
     private List<Socio> listadoSocios = new ArrayList<>();
